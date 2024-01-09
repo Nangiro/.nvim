a8982ca12f6291283f0e126a3ef2074f35cdfe5b
Fernando Kira <61757954+Nangiro@users.noreply.github.com>
2024-01-09 15:13:30 -0300

    Add files via upload
    My lua configs in neovim

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,23 @@
+return {
+  -- tools
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "selene",
+        "luacheck",
+        "shellcheck",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "clangd",
+        "css-lsp",
+        "gopls",
+        "intelephense",
+        "pylyzer",
+        "sqls",
+      })
+    end,
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = { enable = true },
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "fish",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "java",
+        "php",
+        "rust",
+        "scss",
+        "sql",
+        "svelte",
+        "javascript",
+        "typescript",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
